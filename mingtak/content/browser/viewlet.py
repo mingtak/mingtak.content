9e775dfddfccb11cea8154b57212d237307a120c
Andy Fang <andy@mingtak.com.tw>
2018-08-08 17:55:27 +0800

    update 20180808-1

--- mingtak/content/browser/viewlet.py
+++ mingtak/content/browser/viewlet.py
@@ -1,8 +1,8 @@
 #-*- coding:utf-8 -*-
 from zope.interface import Interface
 from five import grok
-from plone.app.layout.viewlets.interfaces import IPortalHeader, IAboveContentTitle
-#IAboveContentTitle, IBelowContentBody
+from plone.app.layout.viewlets.interfaces import IPortalHeader, IAboveContentTitle, IAboveContentBody, IBelowContentBody
+#IAboveContentTitle, IBelowContentBody, IAboveContentBody, IBelowContentBody
 
 
 #設定viewlet介面、pt檔目錄
@@ -13,3 +13,11 @@ grok.templatedir('viewlet_templates')
 class MingtakHomeSlider(grok.Viewlet):
     grok.viewletmanager(IPortalHeader)
 #下列可再新增viewlet, 可用的interface可查詢 plone.app.layout.viewlets.interfaces
+
+
+class GoogleAdAboveDocumentBody(grok.Viewlet):
+    grok.viewletmanager(IAboveContentBody)
+
+
+class GoogleAdBelowDocumentBody(grok.Viewlet):
+    grok.viewletmanager(IBelowContentBody)
